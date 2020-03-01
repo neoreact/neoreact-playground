97a241aaf8b2b87691cdd24ff0ca76629c7b12b4
react neo <neoreact32@gmail.com>
2020-03-01 13:30:34 +1100

    clean up

--- src/neoreact/src/core.tsx
+++ src/neoreact/src/core.tsx
@@ -2,61 +2,63 @@ type JSXReturn = (props: any) => JSX.Element;
 type ComponentType = JSX.Element | JSXReturn;
 
 export interface State<Type, PassedProps> {
-    type: Type;
-    props: PassedProps;
+  type: Type;
+  props: PassedProps;
 }
 
 export type DOMElementIdentifier = string;
 
 export interface Zone {
-    name: string;
-    target: DOMElementIdentifier;
-    component: ComponentType;
-    order: number;
+  name: string;
+  target: DOMElementIdentifier;
+  component: ComponentType;
+  order: number;
 }
 
 export interface Service<StateProps> {
-    name: string;
-    // TODO: Should this just be part of the zones & we add routes?
-    component: ComponentType;
-    // State object
-    state: State<string, StateProps>;
-    // Zones. Things we'll render into
-    zones: Zone[];
-    // How this service will communicate with others (eg/ maybe it is via some third party extension just to allow some kind of encrypted message channel etc. Just allows user freedom if it's able to be modified per service & globally)
-    communicationMethod?: 'redux-saga' | 'other' | string;
-    required: boolean;
+  name: string;
+  // State object
+  state: State<string, StateProps>;
+  // Zones. Things we'll render into
+  zones: Zone[];
+  // How this service will communicate with others (eg/ maybe it is via some third party extension just to allow some kind of encrypted message channel etc. Just allows user freedom if it's able to be modified per service & globally)
+  communicationMethod?: "redux-saga" | "other" | string;
+  required: boolean;
 }
 
-type LifeCycleHooks = "pre-creation" | "post-creation" | "pre-render" | "post-render";
+type LifeCycleHooks =
+  | "pre-creation"
+  | "post-creation"
+  | "pre-render"
+  | "post-render";
 
 export interface Extension {
-    name: string;
-    lifecycleDesired: LifeCycleHooks;
-    type: keyof NeoExtension;
-    func: (instance: NeoExtension) => void;
+  name: string;
+  lifecycleDesired: LifeCycleHooks;
+  type: keyof NeoExtension;
+  func: (instance: NeoExtension) => void;
 }
 
 export interface Configuration<StateProps> {
-    services: Service<StateProps>[];
-    target: DOMElementIdentifier;
-    extensions?: { [key: string]: Extension };
-    debug?: boolean;
-    component: ComponentType;
+  services: Service<StateProps>[];
+  target: DOMElementIdentifier;
+  extensions?: { [key: string]: Extension };
+  debug?: boolean;
+  component: ComponentType;
 }
 
 export interface NeoExtension {
-    // This is only exposed to the extensions
-    // This only supports ONE state handler. Do we want to support multiple?
-    stateHandler?: (state: State<string, any>[]) => void;
+  // This is only exposed to the extensions
+  // This only supports ONE state handler. Do we want to support multiple?
+  stateHandler?: (state: State<string, any>[]) => void;
 }
 
 export interface NeoReact<StateProps> {
-    // Add a service dynamically.
-    add: (service: Service<StateProps>) => void;
-    // Conduct the creation of the app. (?)
-    create: () => void;
-    // Initialize the application, this should pretty much run at the ReactDOM.render() time.
-    // constructor: (config: Configuration<StateProps>) => NeoReact<StateProps>;
-    debug?: boolean;
+  // Add a service dynamically.
+  add: (service: Service<StateProps>) => void;
+  // Conduct the creation of the app. (?)
+  create: () => void;
+  // Initialize the application, this should pretty much run at the ReactDOM.render() time.
+  // constructor: (config: Configuration<StateProps>) => NeoReact<StateProps>;
+  debug?: boolean;
 }
